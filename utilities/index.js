0e28628feb2f21db79fc5005e66759fe10f59523
CopeOnline <Cop21004@byui.edu>
2024-03-16 10:55:45 -0600

    wave report fixes

--- utilities/index.js
+++ utilities/index.js
@@ -36,7 +36,7 @@ Util.buildClassificationGrid = async function (data) {
       grid += '<a href="../../inv/detail/' + vehicle.inv_id +
         '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model +
         'details"><img src="' + vehicle.inv_thumbnail +
-        '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+        '" alt="' + vehicle.inv_make + ' ' + vehicle.inv_model +
         ' on CSE Motors" ></a>'
       grid += '<div class="namePrice">'
       grid += '<hr >'
@@ -67,7 +67,7 @@ Util.buildDetailsView = async function (data) {
     data.forEach(vehicle => {
       grid += '<li>'
       grid += '<img src="' + vehicle.inv_image +
-      '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model +
+      '" alt="' + vehicle.inv_make + ' ' + vehicle.inv_model +
       ' on CSE Motors" >'
       grid += '<div class="vehicleDetails">'
       grid += '<hr >'
