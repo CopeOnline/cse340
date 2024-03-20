dc4bc2ecae13987f36cf4883d4520ffee0f2091b
CopeOnline <Cop21004@byui.edu>
2024-03-19 22:46:43 -0600

    forms and validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,16 @@ const express = require('express')
 const router = new express.Router()
 const accountController = require('../controllers/accountController')
 const utilities = require('../utilities')
+const regValidate = require('../utilities/account-validation')
 
 // Route to login view
 router.get('/login', utilities.handleErrors(accountController.buildLogin))
 
 // Route to registration view
 router.get('/register', utilities.handleErrors(accountController.buildRegister))
+
+// Route to register account
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 
 module.exports = router
